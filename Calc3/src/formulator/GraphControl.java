82161d3a3b783edb200a12def59cd4fdadd85c36
Alan1Mulhall <alan1mulhall@gmail.com>
2014-04-30 14:51:12 +0100

    Graph: remove test code again

--- Calc3/src/formulator/GraphControl.java
+++ Calc3/src/formulator/GraphControl.java
@@ -13,7 +13,7 @@ public class GraphControl {
 		graphlist = graphs;
 		formulas = map;
 		System.out.println("Running graph...");
-		//doGraph(graphlist);
+		doGraph(graphlist);
 	}
 	
 	public void doGraph(final Vector<GraphFunction> graphlist2) {
@@ -26,30 +26,30 @@ public class GraphControl {
 
 		@Override
 		   public void run() {
-			   	//String str = "cos(x)";
-			   	String str = "x(4/3)";
-				//String str = "sin(x)";
-				//String str = "(x+2)(x-(y^7)+cos(2^x))";
-				FormulaElement result = (FormulaElement.parseFormula(str, formulas));
-				//Pass in the root node of the formula to the CartesianFrame constructor
-				double min =-5;
-				double max =5;
-				double incre = 1;
-				Vector<GraphFunction> graphs = new Vector<GraphFunction>();
-				GraphFunction test = new GraphFunction(result, "x", min, max, incre);
-				graphs.add(test);
-			   	String str2 = "cos(x)";
-			   	String str3 = "x(2/3)";
+//			   	//String str = "cos(x)";
+//			   	String str = "x(4/3)";
+//				//String str = "sin(x)";
+//				//String str = "(x+2)(x-(y^7)+cos(2^x))";
+//				FormulaElement result = (FormulaElement.parseFormula(str, formulas));
+//				//Pass in the root node of the formula to the CartesianFrame constructor
+//				double min =-5;
+//				double max =5;
+//				double incre = 1;
+//				Vector<GraphFunction> graphs = new Vector<GraphFunction>();
+//				GraphFunction test = new GraphFunction(result, "x", min, max, incre);
+//				graphs.add(test);
+//			   	String str2 = "cos(x)";
+//			   	String str3 = "x(2/3)";
+//
+//				FormulaElement result2 = (FormulaElement.parseFormula(str2, formulas));
+//				FormulaElement result3 = (FormulaElement.parseFormula(str3, formulas));
+//
+//				GraphFunction test2 = new GraphFunction(result2, "x", min, max, incre);
+//				GraphFunction test3 = new GraphFunction(result3, "x", min, max, incre);
+//				graphs.add(test2);
+//				graphs.add(test3);
 
-				FormulaElement result2 = (FormulaElement.parseFormula(str2, formulas));
-				FormulaElement result3 = (FormulaElement.parseFormula(str3, formulas));
-
-				GraphFunction test2 = new GraphFunction(result2, "x", min, max, incre);
-				GraphFunction test3 = new GraphFunction(result3, "x", min, max, incre);
-				graphs.add(test2);
-				graphs.add(test3);
-
-				CartesianFrame frame = new CartesianFrame(graphs, false, true, true);
+				CartesianFrame frame = new CartesianFrame(graphlist, false, true, true);
 				frame.showUI();
 		   }
 		  });
