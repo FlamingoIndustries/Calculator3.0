ff5a96d72c9906294308ca171a0b6f7abdc9f0b5
Higgles <thomas.higgins.2@ucdconnect.ie>
2014-04-30 22:44:03 +0100

    Name on top

--- Calc3/src/formulator/GraphControl.java
+++ Calc3/src/formulator/GraphControl.java
@@ -2,8 +2,6 @@ package formulator;
 
 import java.util.HashMap;
 import java.util.Vector;
-
-import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class GraphControl {
--- Calc3/src/formulator/Splash.java
+++ Calc3/src/formulator/Splash.java
@@ -1,3 +1,12 @@
+/*
+ * Group name: All Caps Bats
+ * Team Members: 
+ * Alan Mulhall 10335911
+ * Barbara DeKegel 11702369
+ * Stephen Read 11312696
+ * Thomas Higgins 11322981 
+ */
+
 package formulator;
  
 import org.eclipse.swt.SWT;
